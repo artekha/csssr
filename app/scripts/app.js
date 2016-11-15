8ef77b7b7680023274ddfbfd8491909f6b876462
Andrew Artekha <andrewartekha@gmail.com>
2016-11-15 18:41:16 +0200

    layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,23 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
+class NerrativeArea {
 
-$(() => {
-	svg4everybody();
+	constructor(options) {
+
+		this.area = options.textarea;
+		this.area.addEventListener('input', this.resizeArea.bind(this))
+
+	}
+
+	resizeArea(e) {
+
+		const area = e.target;
+
+		if (area.scrollHeight > area.offsetHeight) area.style.height = `${area.scrollHeight}px`
+		console.log(`${area.scrollHeight} ${area.offsetHeight}`);
+
+	}
+
+}
+
+const nerrativeArea = new NerrativeArea({
+	textarea: document.querySelector('textarea')
 });
